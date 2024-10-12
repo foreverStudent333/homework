02c58f0a43bb30e413f70237966a2ceba1992f61
Mokoko <mihaillKHn@yandex.ru>
2024-10-12 03:26:21 +0300

    test commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int one = 1;
+        int two = 2;
+        System.out.println(one + two);
     }
 }
